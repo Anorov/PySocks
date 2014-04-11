cb4fb58b02ea19fff546583695da95a53ce5d6c8
Martin Panter <vadmium  gmailcom>
2014-04-11 03:38:13 +0000

    Use “python2” command for Python 2 scripts

--- test/test.sh
+++ test/test.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Starting proxy servers..."
-python socks4server.py > /dev/null 2>&1 &
-python httpproxy.py > /dev/null 2>&1 &
+python2 socks4server.py > /dev/null 2>&1 &
+python2 httpproxy.py > /dev/null 2>&1 &
 ./mocks start >/dev/null 2>&1 &
 
 sleep 2
 echo "Python 2.x tests"
-python sockstest.py
+python2 sockstest.py
 
 sleep 2
 echo "Python 3.x tests"
 python3 sockstest.py
 
-pkill python > /dev/null 2>&1
+pkill python2 > /dev/null 2>&1
 ./mocks shutdown >/dev/null 2>&1 &
 echo "Finished tests"
