0e480e75cd88032a009de49aeaca0a49702c954c
Anorov <anorov.vorona@gmail.com>
2015-03-28 16:39:59 -0400

    Bump version to 1.5.2

--- socks.py
+++ socks.py
@@ -1,6 +1,6 @@
 """
 SocksiPy - Python SOCKS module.
-Version 1.5.1
+Version 1.5.2
 
 Copyright 2006 Dan-Haim. All rights reserved.
 
@@ -52,7 +52,7 @@ Modifications made by Anorov (https://github.com/Anorov)
 -Various small bug fixes
 """
 
-__version__ = "1.5.1"
+__version__ = "1.5.2"
 
 import socket
 import struct
