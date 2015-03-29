7bae9f46785fde19cb3ffd3d85cd6db183dc4d31
Anorov <anorov.vorona@gmail.com>
2015-03-28 21:39:28 -0400

    Add setup.py required field

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     description = "A Python SOCKS client module. See https://github.com/Anorov/PySocks for more information.",
     url = "https://github.com/Anorov/PySocks",
     license = "BSD",
+    author = "Anorov",
     author_email = "anorov.vorona@gmail.com",
     keywords = ["socks", "proxy"],
     py_modules=["socks", "sockshandler"]
