c6587b80fdcb6adac84d0e3107122e3e67ff4ea5
Felix Yan <felixonmars@archlinux.org>
2020-02-22 01:21:39 +0800

    Fix classifiers warnings in latest python/setuptools
    `Warning: 'classifiers' should be a list, got type 'tuple'`

--- setup.py
+++ setup.py
@@ -32,12 +32,12 @@ setup(
     py_modules=["socks", "sockshandler"],
     install_requires=requirements,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
-    ),
+    ],
 )
